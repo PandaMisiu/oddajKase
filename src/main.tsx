c0a5ab90337cbaf20d574691bd76d6f50e574a19
kambolo <kamil.boloz0407@gmail.com>
2026-05-13 13:23:40 +0200

    Auth Page added including sign in, register and forgot password forms

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import SignInPage from "./pages/AuthPage.tsx";
 import DashboardPage from "./pages/DashboardPage.tsx";
-import SignInPage from "./pages/SignInPage.tsx";
 import GroupPage from "./pages/GroupPage.tsx";
 
 const router = createBrowserRouter([
--- src/pages/SignInPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function SignInPage() {
-  return (
-    <div className="flex h-dvh">
-      <div className="flex-1 bg-brand">SIGN IN</div>
-    </div>
-  );
-}
